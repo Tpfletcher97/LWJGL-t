f8f1eda66fb988d0a5b2b0f46ca85e1aef3a5335
tpfletcher97 <tpfletcher97@gmail.com>
2019-07-06 14:44:02 +1000

    Maven compiler target fix

--- src/main/java/tech/tfletcher/engine/rendering/Renderer.java
+++ src/main/java/tech/tfletcher/engine/rendering/Renderer.java
@@ -7,6 +7,7 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL30.*;
 
 
+
 public class Renderer {
     private ShaderProgram shaderProgram;
     private boolean oldGL = true;
